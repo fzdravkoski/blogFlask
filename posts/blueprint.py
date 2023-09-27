699f00a05bb5ddc0783bf7856bd5ece95e397e10
fzdravkoski <67110192+fzdravkoski@users.noreply.github.com>
2023-09-27 22:06:03 +0200

    /

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,13 +1,30 @@
 from flask import Blueprint
 from flask import render_template
 from flask import request
+from flask import redirect
+from flask import url_for
 from models import *
 from .forms import PostForm
+from app import db
+
+
 posts = Blueprint('posts', __name__, template_folder='templates')
 
-@posts.route('/create')
+@posts.route('/create', methods=['POST','GET'])
 def post_create():
     form = PostForm()
+
+    if request.method == 'POST':
+        title = request.form.get('title')
+        body = request.form.get('body')
+        try:
+            post = Post(title=title, body=body)
+            db.session.add(post)
+            db.session.commit()
+        except:
+            print('You have a mistake.')
+        return redirect(url_for('posts.post_details', slug=post.slug))
+
     return render_template('posts/post_create.html', form = form)
 
 
@@ -15,12 +32,21 @@ def post_create():
 @posts.route('/')
 def post_lists():
     q =  request.args.get('q')
+
     if q:
         posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q))
     else:
-        posts = Post.query.all()
+        posts = Post.query.order_by(Post.created.desc())
 
-    return render_template('posts/posts.html', posts=posts)
+    page = request.args.get('page')
+    if page and page.isdigit():
+        page = int(page)
+    else:
+        page = 1
+
+    pages = posts.paginate(page=page, per_page=2)
+
+    return render_template('posts/posts.html', posts=posts, pages=pages)
 
 
 @posts.route('/<slug>')
@@ -33,3 +59,6 @@ def post_details(slug):
 def tag_detail(slug):
     tag = Tag.query.filter(Tag.slug==slug).first()
     return render_template('posts/tag_detail.html', tag = tag)
+
+
+@posts.route('/<slug>/edit', methods=['POST', 'GET'])
